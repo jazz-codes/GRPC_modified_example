5dc64d0f6a6ad69124602b7ebb4b8bd4c13b0d9f
jazz-codes <152726047+jazz-codes@users.noreply.github.com>
2024-03-22 04:06:20 +0100

    Add files via upload

---
+++ src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -0,0 +1,107 @@
+package io.grpc.examples.helloworld;
+
+/*
+ * Copyright 2015 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import io.grpc.Channel;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.StatusRuntimeException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A simple client that requests a greeting from the {@link HelloWorldServer}.
+ */
+public class HelloWorldClient {
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    public HelloWorldClient(Channel channel) {
+        // 'channel' here is a Channel, not a ManagedChannel, so it is not this code's responsibility to
+        // shut it down.
+
+        // Passing Channels to code makes code easier to test and makes it easier to reuse Channels.
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name, String lastname, int cin, int age, String gender) {
+        logger.info("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder()
+                .setName(name)
+                .setLastname(lastname)
+                .setCin(cin)
+                .setAge(age)
+                .setGender(gender)
+                .build();
+        try {
+            HelloReply response = blockingStub.sayHello(request);
+            logger.info("Greeting: " + response.getMessage());
+
+            response = blockingStub.sayHelloAgain(request);
+            logger.info("Greeting: " + response.getMessage());
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+        }
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting. The second argument is the target server.
+     */
+    public static void main(String[] args) throws Exception {
+        String user = "world";
+        String target = "localhost:50051";
+        if (args.length > 0) {
+            if ("--help".equals(args[0])) {
+                System.err.println("Usage: [name [target]]");
+                System.err.println("");
+                System.err.println("  name    The name you wish to be greeted by. Defaults to " + user);
+                System.err.println("  target  The server to connect to. Defaults to " + target);
+                System.exit(1);
+            }
+            user = args[0];
+        }
+        if (args.length > 1) {
+            target = args[1];
+        }
+
+        // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+        // and reusable. It is common to create channels at the beginning of your application and reuse
+        // them until the application shuts down.
+        //
+        // For the example we use plaintext insecure credentials to avoid needing TLS certificates. To
+        // use TLS, use TlsChannelCredentials instead.
+        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+                .build();
+        try {
+            HelloWorldClient client = new HelloWorldClient(channel);
+            client.greet(user, "Yasmine", 12345678, 21, "Female");
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+    }
+
+	
+
+
